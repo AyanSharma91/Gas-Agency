6c4607caa42c0649d51a3107aeaf83e20466c10c
ROHIT AGRAWAL <64243388+rohit1508-gif@users.noreply.github.com>
2020-12-14 19:14:09 +0530

    Added FormActivity

---
+++ app/src/main/java/com/example/gas_agecy_app/Activities/NewSupplierDetail.kt
@@ -0,0 +1,11 @@
+package com.example.gas_agecy_app.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewSupplierDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_supplier_detail)
+    }
+}
